345b472ffd44a1a3eb6eea1a5c80ff0d0081a05d
christapi2099 <94759708+christapi2099@users.noreply.github.com>
2024-08-06 20:43:36 -0700

    Added LLAMA support base code

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,7 +1,21 @@
 import { NextResponse } from "next/server"
 
 export async function POST(req){
-    const data = await req.json()
-    console.log(data)
+    const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + process.env.OPENROUTER_API_KEY,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "model": "meta-llama/llama-3.1-8b-instruct:free",
+          "messages": [
+            {"role": "user", "content": "Recommend me a simple salami sandwitch recipe."},
+          ]
+        })
+      });
+    
+    const data = await response.json();
+    console.log("Assistant:", data.choices[0].message.content);
     return NextResponse.json({message: 'Hello from server!'})
 }
